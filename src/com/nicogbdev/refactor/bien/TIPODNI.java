4124fd24388c9c8bb12348d4f667dd0ead596909
Nicolás Gabarrón Blaya <nicolasgabarroncip@gmail.com>
2022-04-01 08:56:06 +0200

    add: Punto inicial.

---
+++ src/com/nicogbdev/refactor/bien/TIPODNI.java
@@ -0,0 +1,7 @@
+package com.nicogbdev.refactor.bien;
+
+public enum TIPODNI {
+    DNI,
+    NIE,
+    CIF
+}
---
+++ src/com/nicogbdev/refactor/bien/TipoUltCaracter.java
@@ -0,0 +1,8 @@
+package com.nicogbdev.refactor.bien;
+
+public enum TipoUltCaracter
+{
+    LETRA,
+    NUMERO,
+    AMBOS
+}
