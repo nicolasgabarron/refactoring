4cca87712e6d40899421d47b0c0f15a1589fa2ab
Nicolás Gabarrón Blaya <nicolasgabarroncip@gmail.com>
2022-04-01 10:16:30 +0200

    add: Interfaz 'IdentificationValidate' y clases implementación.

---
+++ src/com/nicogbdev/refactor/bien/interfaces/IdentificationValidate.java
@@ -0,0 +1,5 @@
+package com.nicogbdev.refactor.bien.interfaces;
+
+public interface IdentificationValidate {
+    void validate();
+}
---
+++ src/com/nicogbdev/refactor/bien/utils/CifValidator.java
@@ -0,0 +1,10 @@
+package com.nicogbdev.refactor.bien.utils;
+
+import com.nicogbdev.refactor.bien.interfaces.IdentificationValidate;
+
+public class CifValidator implements IdentificationValidate {
+    @Override
+    public void validate() {
+
+    }
+}
