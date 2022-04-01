bc469068c3d6df813d46c03f3502d8ed1a1f38db
Nicolás Gabarrón Blaya <nicolasgabarroncip@gmail.com>
2022-04-01 09:40:15 +0200

    mod: Cambio nombres de variables.

--- src/com/nicogbdev/refactor/bien/DNI.java
+++ src/com/nicogbdev/refactor/bien/DNI.java
@@ -6,15 +6,16 @@ import java.util.regex.Pattern;
 
 public class DNI {
 
-    public TIPODNI enumTipo;    // tipo de documento
-    public String numDNI;       // identificador del documento
-    public Date fchValidez;     // fecha de validez del documento
+    // PROPIEDADES.
+    public TIPODNI documentType;        // Tipo del documento (DNI, NIE, CIF).
+    public String dniNumberWithLetter;  // Número de documento con letra incluida (ej. 12345678A)
+    public Date expirationDate;         // Fecha de validez del documento.
 
     // construye un DNI
-    public DNI(TIPODNI tipo, String numDNI, Date fchValidez) {
-        this.enumTipo = tipo;           // tipo de documento
-        this.numDNI = numDNI;           // asignamos el DNI
-        this.fchValidez = fchValidez;   // asignamos la fecha de validez
+    public DNI(TIPODNI type, String dniNumberWithLetter, Date expirationDate) {
+        this.documentType = type;
+        this.dniNumberWithLetter = dniNumberWithLetter;
+        this.expirationDate = expirationDate;
     }
 
 
@@ -24,80 +25,80 @@ public class DNI {
     // si pasa algo devuelve 99
     public int validarDNI() {
 
-        switch (this.enumTipo) {
+        switch (this.documentType) {
             case DNI:
 
                 // posibles letras en un DNI
-                String dniChars="TRWAGMYFPDXBNJZSQVHLCKE";
+                String dniChars = "TRWAGMYFPDXBNJZSQVHLCKE";
                 // los primeros 8 caracteres son números
-                String intPartDNI = this.numDNI.trim().replaceAll(" ", "").substring(0, 8);
+                String numericalPartDni = this.dniNumberWithLetter.trim().replaceAll(" ", "").substring(0, 8);
                 // el último es un dígito de control
-                char ltrDNI = this.numDNI.charAt(8);
+                char dniLetter = this.dniNumberWithLetter.charAt(8);
                 // calculamos el módulo de 23 de la parte numérica que debería ser el caracter en esa
                 // posición en la lista de dniChar --> my code Rocks!!!
-                int valNumDni = Integer.parseInt(intPartDNI) % 23;
+                int calculatedLetter = Integer.parseInt(numericalPartDni) % 23;
 
                 // comprobamos que tutto esté bien
-                if (this.numDNI.length()!= 9 || isNumeric(intPartDNI) == false || dniChars.charAt(valNumDni)!= ltrDNI) {
+                if (this.dniNumberWithLetter.length() != 9 || isNumeric(numericalPartDni) == false || dniChars.charAt(calculatedLetter) != dniLetter) {
                     return 0; // algo no se cumple
                 } else {
                     return 1; // to correcto
                 }
 
             case CIF:
-                if (this.numDNI != null) {
-                    final String cifUP = this.numDNI.toUpperCase();
+                if (this.dniNumberWithLetter != null) {
+                    final String cifUpperCase = this.dniNumberWithLetter.toUpperCase();
 
                     // si el primer caracter no es uno de los válidos entonces ya fallamos
-                    if ("ABCDEFGHJKLMNPQRSUVW".indexOf(cifUP.charAt(0)) == -1) {
+                    if ("ABCDEFGHJKLMNPQRSUVW".indexOf(cifUpperCase.charAt(0)) == -1) {
                         return 0; // no cumple el primer char
                     }
 
                     // si no cumple el patrón de CIF fallamos
                     final Pattern mask = Pattern
                             .compile("[ABCDEFGHJKLMNPQRSUVW][0-9]{7}[A-Z[0-9]]{1}");
-                    final Matcher matcher = mask.matcher(cifUP);
+                    final Matcher matcher = mask.matcher(cifUpperCase);
                     if (!matcher.matches()) {
                         return 0; // no cumple la máscara
                     }
 
-                    final char primerCar = cifUP.charAt(0);
-                    final char ultimoCar = cifUP.charAt(cifUP.length() - 1);
+                    final char firstCharacterCif = cifUpperCase.charAt(0);
+                    final char lastCharacterCif = cifUpperCase.charAt(cifUpperCase.length() - 1);
 
 
-                    TipoUltCaracter tipUltCar;
+                    TipoUltCaracter lastCharacterType;
 
-                    // si empiezo por P,Q, S, K o W la última letra tiene que ser una LETRA
-                    if (primerCar == 'P' || primerCar == 'Q' || primerCar == 'S' || primerCar == 'K' || primerCar == 'W') {
-                        tipUltCar = TipoUltCaracter.LETRA;
-                        if (!(ultimoCar >= 'A' && ultimoCar <= 'Z')) {
-                            return 0; // no es una letra
+                    // si empiezo por P,Q, S, K o W la última firstLetterNie tiene que ser una LETRA
+                    if (firstCharacterCif == 'P' || firstCharacterCif == 'Q' || firstCharacterCif == 'S' || firstCharacterCif == 'K' || firstCharacterCif == 'W') {
+                        lastCharacterType = TipoUltCaracter.LETRA;
+                        if (!(lastCharacterCif >= 'A' && lastCharacterCif <= 'Z')) {
+                            return 0; // no es una firstLetterNie
                         }
-                    // si empiezo por A, B, E o H la última letra tiene que ser un número
-                    } else if (primerCar == 'A' || primerCar == 'B' || primerCar == 'E'
-                            || primerCar == 'H') {
-                        tipUltCar = TipoUltCaracter.NUMERO;
-                        if (!(ultimoCar >= '0' && ultimoCar <= '9')) {
+                        // si empiezo por A, B, E o H la última firstLetterNie tiene que ser un número
+                    } else if (firstCharacterCif == 'A' || firstCharacterCif == 'B' || firstCharacterCif == 'E'
+                            || firstCharacterCif == 'H') {
+                        lastCharacterType = TipoUltCaracter.NUMERO;
+                        if (!(lastCharacterCif >= '0' && lastCharacterCif <= '9')) {
                             return 0; // no es un número --> casco!
                         }
-                    // en otro caso la última letra puede ser cualquier cosa
+                        // en otro caso la última firstLetterNie puede ser cualquier cosa
                     } else {
-                        tipUltCar = TipoUltCaracter.AMBOS;
+                        lastCharacterType = TipoUltCaracter.AMBOS;
                     }
 
 
-                    final String digitos = cifUP.substring(1, cifUP.length() - 1);
+                    final String numericPartCif = cifUpperCase.substring(1, cifUpperCase.length() - 1);
 
                     // sumo los pares
                     Integer sumaPares = 0;
-                    for (int i = 1; i <= digitos.length() - 1; i = i + 2) {
-                        sumaPares += Integer.parseInt(digitos.substring(i, i + 1));
+                    for (int i = 1; i <= numericPartCif.length() - 1; i = i + 2) {
+                        sumaPares += Integer.parseInt(numericPartCif.substring(i, i + 1));
                     }
 
                     // sumo los impares
                     Integer sumaImpares = 0;
-                    for (int i = 0; i <= digitos.length() - 1; i = i + 2) {
-                        Integer cal = Integer.parseInt(digitos.substring(i, i + 1)) * 2;
+                    for (int i = 0; i <= numericPartCif.length() - 1; i = i + 2) {
+                        Integer cal = Integer.parseInt(numericPartCif.substring(i, i + 1)) * 2;
                         if (cal.toString().length() > 1) {
                             cal = Integer.parseInt(cal.toString().substring(0, 1))
                                     + Integer.parseInt(cal.toString().substring(1, 2));
@@ -109,42 +110,42 @@ public class DNI {
                     final Integer total = sumaPares + sumaImpares;
 
                     // calculo el número de control
-                    Integer numControl = 10 - (total % 10);
+                    Integer controlNumber = 10 - (total % 10);
 
-                     /*if (numControl == 10){
-                     numControl = 0;
+                     /*if (controlNumber == 10){
+                     controlNumber = 0;
                      }*/
-                    int pos = numControl == 10? 0:numControl;
-                    final char carControl = "JABCDEFGHI".charAt(pos);
+                    int position = controlNumber == 10 ? 0 : controlNumber;
+                    final char controlCharacter = "JABCDEFGHI".charAt(position);
 
                     // con el número de control calculado validamos
-                    if (tipUltCar == TipoUltCaracter.NUMERO) {
+                    if (lastCharacterType == TipoUltCaracter.NUMERO) {
 
-                        final Integer ultCar = Integer.parseInt(Character
-                                .toString(ultimoCar));
-                        if (pos != ultCar.intValue()) {
+                        final Integer lastCharacter = Integer.parseInt(Character
+                                .toString(lastCharacterCif));
+                        if (position != lastCharacter.intValue()) {
 
                             return 0; // NOK
                         }
 
-                    } else if (tipUltCar == TipoUltCaracter.LETRA) {
-                        if (carControl != ultimoCar) {
+                    } else if (lastCharacterType == TipoUltCaracter.LETRA) {
+                        if (controlCharacter != lastCharacterCif) {
                             return 0; // NOK
                         }
 
                     } else {
                         // find all occurrences forward
-                        Integer ultCar = -1;
+                        Integer lastCharacter = -1;
 
-                        ultCar = "JABCDEFGHI".indexOf(ultimoCar);
+                        lastCharacter = "JABCDEFGHI".indexOf(lastCharacterCif);
 
-                        if (ultCar < 0){
-                            ultCar = Integer.parseInt(Character.toString(ultimoCar));
-                            if (pos != ultCar.intValue()) {
+                        if (lastCharacter < 0) {
+                            lastCharacter = Integer.parseInt(Character.toString(lastCharacterCif));
+                            if (position != lastCharacter.intValue()) {
                                 return 0; // NOK
                             }
                         }
-                        if ((pos != ultCar.intValue()) && (carControl != ultimoCar)) {
+                        if ((position != lastCharacter.intValue()) && (controlCharacter != lastCharacterCif)) {
                             return 0; // NOK
                         }
                     }
@@ -153,44 +154,44 @@ public class DNI {
                 return 0; //NOK
 
             case NIE:
-                boolean esValido = false;
+                boolean isValid = false;
                 int i = 1;
-                int caracterASCII = 0;
-                char letra = ' ';
-                int miNIE = 0;
-                int resto = 0;
-                char[] asignacionLetra = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X','B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
+                int characterIndexASCII = 0;
+                char firstLetterNie = ' ';
+                int numericalPartNie = 0;
+                int resto = 0; // Refactorizar a posterior
+                char[] firstLetters = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
 
-                String nie = this.numDNI;
+                String nie = this.dniNumberWithLetter;
 
-                if(nie.length() == 9 && Character.isLetter(nie.charAt(8))
-                        && nie.substring(0,1).toUpperCase().equals("X")
-                        || nie.substring(0,1).toUpperCase().equals("Y")
-                        || nie.substring(0,1).toUpperCase().equals("Z")) {
+                if (nie.length() == 9 && Character.isLetter(nie.charAt(8))
+                        && nie.substring(0, 1).toUpperCase().equals("X")
+                        || nie.substring(0, 1).toUpperCase().equals("Y")
+                        || nie.substring(0, 1).toUpperCase().equals("Z")) {
 
                     do {
-                        caracterASCII = nie.codePointAt(i);
-                        esValido = (caracterASCII > 47 && caracterASCII < 58);
+                        characterIndexASCII = nie.codePointAt(i);
+                        isValid = (characterIndexASCII > 47 && characterIndexASCII < 58);
                         i++;
-                    } while(i < nie.length() - 1 && esValido);
+                    } while (i < nie.length() - 1 && isValid);
                 }
 
-                if(esValido && nie.substring(0,1).toUpperCase().equals("X")) {
-                    nie = "0" + nie.substring(1,9);
-                } else if(esValido && nie.substring(0,1).toUpperCase().equals("Y")) {
-                    nie = "1" + nie.substring(1,9);
-                } else if(esValido && nie.substring(0,1).toUpperCase().equals("Z")) {
-                    nie = "2" + nie.substring(1,9);
+                if (isValid && nie.substring(0, 1).toUpperCase().equals("X")) {
+                    nie = "0" + nie.substring(1, 9);
+                } else if (isValid && nie.substring(0, 1).toUpperCase().equals("Y")) {
+                    nie = "1" + nie.substring(1, 9);
+                } else if (isValid && nie.substring(0, 1).toUpperCase().equals("Z")) {
+                    nie = "2" + nie.substring(1, 9);
                 }
 
-                if(esValido) {
-                    letra = Character.toUpperCase(nie.charAt(8));
-                    miNIE = Integer.parseInt(nie.substring(1,8));
-                    resto = miNIE % 23;
-                    esValido = (letra == asignacionLetra[resto]);
+                if (isValid) {
+                    firstLetterNie = Character.toUpperCase(nie.charAt(8));
+                    numericalPartNie = Integer.parseInt(nie.substring(1, 8));
+                    resto = numericalPartNie % 23;
+                    isValid = (firstLetterNie == firstLetters[resto]);
                 }
 
-                if (esValido) {
+                if (isValid) {
                     return 1; // todo OK
                 } else {
                     return 0; // algo NOK
