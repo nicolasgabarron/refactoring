8a72fb2f26d34b3ccefc2c3c0d20b3575c2497ae
Nicolás Gabarrón Blaya <nicolasgabarroncip@gmail.com>
2022-04-04 19:31:18 +0200

    add: Propiedades al modelo CIF.

--- src/com/nicogbdev/refactor/bien/models/CIF.java
+++ src/com/nicogbdev/refactor/bien/models/CIF.java
@@ -5,10 +5,16 @@ import java.util.Date;
 public class CIF extends IdentificationDocument {
     private String cifNumberWithLetters;
     private Date expirationDate;
+    private char firstCharacter;
+    private char lastCharacter;
+    private int numericPart;
 
     public CIF(String cifNumberWithLetters, Date expirationDate) {
         this.cifNumberWithLetters = cifNumberWithLetters.toUpperCase();
         this.expirationDate = expirationDate;
+        this.setNumericPart(); // En caso de tener formato incorrecto, esta sentencia daría error.
+        this.setFirstCharacter();
+        this.setLastCharacter();
     }
 
     // Getters y Setters.
@@ -28,15 +34,27 @@ public class CIF extends IdentificationDocument {
         this.expirationDate = expirationDate;
     }
 
+    public void setFirstCharacter() {
+        this.firstCharacter = cifNumberWithLetters.charAt(0);
+    }
+
+    public void setLastCharacter() {
+        this.lastCharacter = cifNumberWithLetters.charAt(cifNumberWithLetters.length() - 1);
+    }
+
+    public void setNumericPart() {
+        this.numericPart =  Integer.parseInt(cifNumberWithLetters.substring(1, cifNumberWithLetters.length() - 1));
+    }
+
     public char getFirstCharacter() {
-        return cifNumberWithLetters.charAt(0);
+        return this.firstCharacter;
     }
 
     public char getLastCharacter() {
-        return cifNumberWithLetters.charAt(cifNumberWithLetters.length() - 1);
+        return this.lastCharacter;
     }
 
     public int getNumericPartCIF() {
-        return Integer.parseInt(cifNumberWithLetters.substring(1, cifNumberWithLetters.length() - 1));
+        return this.numericPart;
     }
 }
